1ed60acc1d575416f152a0a0945d4779aa2a8786
Persis <persis.randolph@gmail.com>
2021-07-08 13:33:14 -0500

    change host url in ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-13-59-184-112.us-east-2.compute.amazonaws.com',
+      host: 'ec2-3-131-151-82.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/little-victories-server-key.pem',
       ref: 'origin/main',
       repo: 'git@github.com:ryguy-coder-guys/LittleVictoriesServer.git',
