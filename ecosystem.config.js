28276a758dc8fe83048561bef9d45fdeeeaf6bdd
Persis <persis.randolph@gmail.com>
2021-07-08 13:28:49 -0500

    changed ecosystem.config file key name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-13-59-184-112.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/little-victories-key.pem',
+      key: '~/.ssh/little-victories-server-key.pem',
       ref: 'origin/main',
       repo: 'git@github.com:ryguy-coder-guys/LittleVictoriesServer.git',
       path: '/home/ubuntu/LittleVictoriesServer',
